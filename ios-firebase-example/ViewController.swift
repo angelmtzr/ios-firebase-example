99ec152d8075c6c9a71bb8b44a0647c69e38cfcf
angelmtzr <0235347@up.edu.mx>
2022-09-28 20:24:04 -0500

    Initial Commit

---
+++ ios-firebase-example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-firebase-example
+//
+//  Created by Alumno on 28/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
